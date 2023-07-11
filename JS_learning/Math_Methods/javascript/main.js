d70c923e132d35e2bfa915137db0633b22f6e309
kibetstephanie <stephkiby@gmail.com>
2023-07-11 22:44:17 +0300

    Updated folder

---
+++ JS_learning/Math_Methods/javascript/main.js
@@ -0,0 +1,11 @@
+//Math Properties
+
+console.log(Math.PI); //shows value of PI
+console.log(Math.trunc(Math.PI)); //removes the decimal places 
+console.log(Math.round(Math.PI)); //rounds off to the nearest integer
+console.log(Math.ceil(Math.PI)); // round up to the next integer
+console.log(Math.floor(Math.PI)); //rounds down to the nearest integer
+console.log(Math.pow(2,4)); // like saying 2 to power 4
+console.log(Math.min(2,6,34,0.9)); //shows the smallest value in the list
+console.log(Math.random()); //gives a random number between 0 and 1
+console.log(Math.floor(Math.random() * 10) + 1);
