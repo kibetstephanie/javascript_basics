ed7f08686e3246c63db6ab81be0dc8b40616f438
kibetstephanie <stephkiby@gmail.com>
2023-07-12 12:37:26 +0300

    Updated folder

--- JS_learning/If_Statements/javascript/main.js
+++ JS_learning/If_Statements/javascript/main.js
@@ -1,6 +1,6 @@
-//If Statements
+//Conditional: If Statements
 
-let soup = "chicken noodle soup"
+let soup = "chicken noodle soup";
 let reply;
 let customerIsBanned = false;
 let crackers = true;
@@ -16,4 +16,56 @@ else{
     reply = `Sorry we're out of ${soup}`;
 }
 console.log(reply);
+
+
+
+// Example 2: Test Scores
+
+let testScores = 59;
+let grade;
+let collegeStudent = true;
+
+if (testScores >= 90){
+    grade = "A";
+} else if (testScores >= 80){
+    grade = "B";
+} else if (testScores >= 70){
+    grade = "C"
+} else if (testScores >= 60){
+    grade = "D"
+} else { 
+    if (collegeStudent){
+        grade = "Pass"
+    } else {
+        grade = "Fail"
+    }
+}
+
+console.log(grade);
+
+// Example 3: Decision Tree Game (Rock paper scissors) Try on my own
+
+/*
+if (playerOne === computer){
+    //tie game
+} else if (playerOne === "rock"){
+    if (computer === "paper"){
+        //computer wins
+    } else {
+        //playerOne wins
+    }
+} else if (playerOne === "paper"){
+    if (computer === "scissors"){
+        //computer wins
+    } else {
+        //playerOne wins
+    }
+} else {
+    if (computer === "rock"){
+        //computer wins
+    }
+    else {
+        //playerOne wins
+    }
+}*/
 
