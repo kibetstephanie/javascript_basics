fdd6de1224a65232f57bc195033a7ee755a3ee8d
kibetstephanie <stephkiby@gmail.com>
2023-07-13 18:47:55 +0300

    Created loops directory

---
+++ JS_learning/Loops/javascript/main.js
@@ -0,0 +1,38 @@
+//Loops
+
+let myNumber = 0;
+
+//while loop
+while(myNumber < 50){
+    myNumber += 2;
+    console.log(myNumber);
+    
+}
+
+// do-while loop
+do {
+
+console.log(myNumber);
+} while (myNumber < 50) //a do loop would have to execute this block at least once
+
+//for loop
+for (let i = 0; i<=10; i++){
+    console.log(i); 
+}
+
+//example
+let myName = "Stephanie";
+let counter = 0;
+let myLetter;
+
+while (counter <= 8){ 
+    myLetter = myName[counter];
+    console.log(myLetter);
+    if(counter === 1){
+        counter +=2;
+        continue;
+    }
+
+    if (myLetter === "i") break;
+    counter++; 
+}
